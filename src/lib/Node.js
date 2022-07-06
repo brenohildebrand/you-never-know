85ad5d3c76f30e4630a1336be43d40c39dd76da9
Breno Hildebrand <brenoh1ldebr4nd@pm.me>
2022-07-06 19:00:34 -0300

    [DEV] MetaCommit

--- src/lib/Node.js
+++ src/lib/Node.js
@@ -4,10 +4,22 @@ export default class Node {
   constructor({
     title = 'Untitled',
     description = '',
+    parent = undefined,
   }) {
+    /* Make sure you got a parent, otherwise throws an error */
+    if( parent === undefined ) {
+      /* TODO: Error Handling */
+    }
+
+    // Metadata
     this.id = genUniqueID();
     this.title = title;
     this.description = description;
+    
+    // Parent and Children
+    this.parent = parent;
     this.children = [];
+
+    // More Properties and Features
   }
 }
--- src/lib/Storage.js
+++ src/lib/Storage.js
@@ -1,29 +1,76 @@
 export default class Storage {
+  // Static properties are macros that are going to be used in this class and
+  // don't need to be recreated for each instance.
+  static KEY_ROOTS = 'ROOTS';
+  static STATUS_NORMAL = 'NORMAL';
+  static STATUS_LOADING = 'LOADING';
+  static STATUS_SAVING = 'SAVING';
+
+  /* Private Properties */
   #STATUS;
 
   constructor() {
-    // The roots of each isolated 
-    this.roots = new Object();
+    // Initialize the status
+    this.#STATUS = STATUS_NORMAL;
+    
+    // Get the roots from the local storage.
+    this._getRoots();
   }
 
   // This function does not need to be async, but once we use a database like
   // MongoDB we'll need to treat this as async. Considering it's better for
   // scalability, let's stick with async for now.
-  async init() {
+  async save({ roots = 'undefined' }) {
+    // Save the current state of the storage object in the localStorage.
+    // If there's any error in the app, don't save anything. Keep the last save.
+
+    // If roots are undefined, handle the error.
+    if(roots == 'undefined') {
+      /* TODO: Error Handling */
+    }
+
+    // Set status to STATUS_SAVING.
+    this.#STATUS = STATUS_SAVING;
+
+    // Save roots to local storage.
+    window.localStorage.setItem(KEY_ROOTS, roots);
+    
+    // Reset status to STATUS_NORMAL.
+    this.#STATUS = STATUS_NORMAL;
+  }
+
+  // This function does not need to be async, but once we use a database like
+  // MongoDB we'll need to treat this as async. Considering it's better for
+  // scalability, let's stick with async for now.
+  async _getRoots() {
     // Get the state from the storage object in the localStorage.
     // Should be called just once, unless there's some error.
-    this.#STATUS = 'loading';
-  }
+    
+    // Set status to STATUS_LOADING.
+    this.#STATUS = STATUS_LOADING;
+    
+    // Get roots from local storage.
+    let roots = window.localStorage.getItem(KEY_ROOTS);
 
-  // This function does not need to be async, but once we use a database like
-  // MongoDB we'll need to treat this as async. Considering it's better for
-  // scalability, let's stick with async for now.
-  async save() {
-    // Save the current state of the storage object in the localStorage.
-    this.#STATUS = 'saving';
+    // If the roots were not created yet, create'em.
+    if(roots === null) {
+      roots = {
+        version: 
+      }
+    }
+
+    // Reset status to STATUS_NORMAL.
+    this.#STATUS = STATUS_NORMAL;
+
+    // Update the roots property
+    this.roots = roots;
   }
 
   get status() {
     return this.#STATUS;
+  }
+
+  get roots() {
+    return this.roots;
   }
 }
