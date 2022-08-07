611741eaf514f0e71ae32e51b0cc2d6cfe2b0ba4
Breno Hildebrand <hello@brenoh1ldebr4nd.com>
2022-08-07 20:33:02 -0300

    [DEV] MetaCommit

---
+++ app/packages/core/create.mjs
@@ -0,0 +1,11 @@
+// Imports
+import nodes from './nodes.mjs';
+
+// Exports
+export default create;
+
+// Constants
+const create = {
+  genID: ,
+  call: ,
+};
--- src/packages/core/node/generateIdentity.js
+++
@@ -1,7 +0,0 @@
-export default (function _generateIdentity () {
-  let id = 0;
-  
-  return function generateIdentity () {
-    return id++;
-  }
-})();
